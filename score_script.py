7b4e91ca45eea6e4be223a7a1149c99067fac1aa
matteocao <38946719+matteocao@users.noreply.github.com>
2024-07-26 10:48:48 +0200

    Add files via upload

---
+++ score_script.py
@@ -0,0 +1,59 @@
+import json
+import os
+from .sensors import Sensors, FullExample
+
+# Get the directory where the script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Build the path to the JSON file
+file_name1 = os.path.join(script_dir, "submission_sumayyeh_only.json")
+file_name2 = os.path.join(script_dir, "submission_sumayyeh_with_us.json")
+
+
+def find_file(directory_path, file_name):
+    # Create the full path to the file within the current working directory
+    full_path = os.path.join(os.getcwd(), directory_path, file_name)
+    return os.path.isfile(full_path)
+
+
+directory_path_train = "data/training"
+directory_path_eval = "data/evaluation"
+directory_path_test = "test_data/evaluation"
+directories = [directory_path_train, directory_path_eval, directory_path_test]
+
+# test find_file
+file_name_example = "1f876c06" + ".json"
+print(file_name_example)
+print(find_file(directory_path=directory_path_eval, file_name=file_name_example))
+
+# start sensors
+sensor_train = Sensors(os.path.join("data", "training"))
+sensor_eval = Sensors(os.path.join("data", "evaluation"))
+sensor_test = Sensors(os.path.join("test_data", "evaluation"))
+sensors = [sensor_train, sensor_eval, sensor_test]
+
+
+def score(file_name):
+    file = open(file_name, "r")
+    data = json.load(file)
+    attempted, score = 0, 0
+    for task_name in data:
+        file_name = task_name + ".json"
+        for i, directory_path in enumerate(directories):
+            if find_file(directory_path, file_name):
+                attempted += 1
+                local_score = 0
+                examples, _ = sensors[i].load_single_task(task_name)
+                full_example: FullExample = examples[0]
+                for j, pair in enumerate(full_example.test):
+                    grid_expected = pair.output.to_list()
+                    if (data[task_name][j]["attempt_1"] == grid_expected) or (
+                        data[task_name][j]["attempt_2"] == grid_expected
+                    ):
+                        local_score += 1
+                score += local_score / len(full_example.test)
+    return attempted, score, score / attempted
+
+
+print(score(file_name1))
+print(score(file_name2))
